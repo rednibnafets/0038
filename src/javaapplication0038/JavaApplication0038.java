dbb3722c147403d1c4f3a398ce12069b3d479f52
stefan <stefan@brunhilde>
2015-12-02 15:26:34 +0000

    01.1 - comments

--- src/javaapplication0038/JavaApplication0038.java
+++ src/javaapplication0038/JavaApplication0038.java
@@ -18,6 +18,7 @@ public class JavaApplication0038 {
     public static void main(String[] args) {
         // TODO code application logic here
         // more comments
+        // add
     }
     
 }
