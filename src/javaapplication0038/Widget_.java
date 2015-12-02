624479f93912a69445632695c2f480381a31ac0a
stefan <stefan@silverdrop.trillium.lan>
2015-12-02 17:36:56 +0000

    02 - Widget_ class added

---
+++ src/javaapplication0038/Widget_.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication0038;
+
+/**
+ *
+ * @author stefan
+ */
+public class Widget_ {
+    
+    private int w ;
+    
+    public Widget_()
+    {
+        w = 0 ;
+    }
+    
+    public Widget_ print()
+    {
+        System.out.println(w) ;
+        return this;
+    }
+    
+}
