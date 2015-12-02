051fbfef26ed51aff1e550475d46cbd86a79fda1
stefan <stefan@brunhilde>
2015-12-02 15:15:42 +0000

    01 - start

---
+++ src/javaapplication0038/JavaApplication0038.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javaapplication0038;
+
+/**
+ *
+ * @author stefan
+ */
+public class JavaApplication0038 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
