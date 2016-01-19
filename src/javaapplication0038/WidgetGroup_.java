fc55f43018aa1308a00d86e26125235d27ce6b28
stefan <stefan@brunhilde>
2016-01-19 12:54:44 +0000

    04. - class WidgetGroup_ added

--- src/javaapplication0038/WidgetGroup_.java
+++ src/javaapplication0038/WidgetGroup_.java
@@ -6,10 +6,38 @@
 
 package javaapplication0038;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author stefan
  */
 public class WidgetGroup_ {
+    
+    private ArrayList< Widget_ > lw ;
+    //private final double pi = 3.1415624 ; 
+    
+    public WidgetGroup_ () 
+    {
+         lw = new ArrayList<>() ;   
+    }
+    
+    public WidgetGroup_ addWidget(Widget_ newWidget) 
+    {
+        lw.add(newWidget) ;
+        return this ;
+    }
+    
+    // calculate the total area of all Widget_
+    public int sumOfAreas()
+    {
+         int area = 0 ;
+         for ( Widget_ w : lw )
+         {
+            area += ( w.getLength() * w.getWidth() ) ;
+         }
+         
+         return area ;
+    }
     
 }
